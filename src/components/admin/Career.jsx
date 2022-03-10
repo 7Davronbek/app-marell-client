dc7c7978c2da84408baf805c616a1ab00f20af1b
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-03-10 18:01:17 +0500

    marell

--- src/components/admin/Career.jsx
+++ src/components/admin/Career.jsx
@@ -10,42 +10,23 @@ const Career = () => {
 
                     <div className="row">
                         <div className="col-lg-7">
-                            <div className="card">
-                                <div className="card-body">
-                                    <div className="d-flex align-items-center justify-content-between w-100">
-                                        <h2>Lv1</h2>
-                                        <h3>2%</h3>
-                                        <h4>5000 Р/ 500 Р + подписка</h4>
-                                    </div>
-                                    <div className="d-flex align-items-center justify-content-between w-100">
-                                        <h2>Lv2</h2>
-                                        <h3>3%</h3>
-                                        <h4>100 000 P</h4>
-                                    </div>
-                                    <div className="d-flex align-items-center justify-content-between w-100">
-                                        <h2>Lv3</h2>
-                                        <h3>3%</h3>
-                                        <h4>250 000 Р</h4>
-                                    </div>
-                                </div>
-                            </div>
 
                             <table className="table">
                                 <tbody>
                                     <tr>
                                         <th><h2>Lv1</h2></th>
-                                        <th><h3>2%</h3></th>
-                                        <th><h4>5000 Р/ 500 Р + подписка</h4></th>
+                                        <th className='text-center'><h3>2%</h3></th>
+                                        <th className='text-right'><h4>5000 Р/ 500 Р + подписка</h4></th>
                                     </tr>
                                     <tr>
                                         <th><h2>Lv2</h2></th>
-                                        <th><h3>3%</h3></th>
-                                        <th><h4>100 000 P</h4></th>
+                                        <th className='text-center'><h3>3%</h3></th>
+                                        <th className='text-right'><h4>100 000 P</h4></th>
                                     </tr>
                                     <tr>
                                         <th><h2>Lv1</h2></th>
-                                        <th><h3>3%</h3></th>
-                                        <th><h4>250 000 P</h4></th>
+                                        <th className='text-center'><h3>3%</h3></th>
+                                        <th className='text-right'><h4>250 000 P</h4></th>
                                     </tr>
                                 </tbody>
                             </table>
@@ -54,7 +35,7 @@ const Career = () => {
                         <div className="col-lg-5">
                             <p>При активации/получении Lv1 Партнер получает 2% от покупок/подписки своей первой линии 
 
-При достижении Lv2 Партнер получает 3% от первой линии и разницу между своим процентом и процентом партнера первой линии: 3-2=1% с глубины</p>
+                                При достижении Lv2 Партнер получает 3% от первой линии и разницу между своим процентом и процентом партнера первой линии: 3-2=1% с глубины</p>
                         </div>
                     </div>
 
