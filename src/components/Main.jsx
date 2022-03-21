cbc8788d83c5d7d8b4fd45486cd5d05fc2d0db04
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-03-21 22:42:09 +0500

    amarell

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import LeftLayout from './LeftLayout';
 
 const Main = () => {
+    const [catalog, setCatalog] = useState(false)
     return (
         <div className="main">
             <div className="rightSide">
 
                 <LeftLayout>
 
-                    <h5 className='mb-4'><span className="blue">Top</span> Men</h5>
+                    <div className="searchWrap shadows">
+                        <button onClick={() => setCatalog(!catalog)} className="btn">ВСЕ <span><img src="/assets/icon/down.svg" alt="" /></span>
+                            <div onMouseLeave={() => setCatalog(false)} className={`buttonWrap ${catalog ? 'active' : ''}`}>
+                                <h6>Женская одежда</h6>
+                                <h6>Мужская одежда</h6>
+                                <h6>Канцтовары</h6>
+                                <h6>Продукты для здоровья</h6>
+                                <h6>Украшения</h6>
+                                <h6>Косметика</h6>
+                                <h6>Косметика</h6>
+                                <h6>Косметика</h6>
+                                <h6>Косметика</h6>
+                            </div>
+                        </button>
+                        <input type="text" placeholder='Я ищу...' className="form-control" />
+                        <p><span><img src="/assets/icon/search.svg" alt="" /></span> поиск</p>
+                    </div>
+
+                    <div className="mainHeader">
+
+                        <AliceCarousel className='myCarousel' responsive={{
+                            0: {
+                                items: 1
+                            }
+                        }} mouseTracking autoPlay={true} autoPlayInterval={3500} disableDotsControls={true} infinite={true} >
+
+                            <div className="carouselWrap ">
+                                <div className="myCard">
+                                    <img src="/assets/image/mainHeader.png" alt="" className="w-100" />
+                                </div>
+                            </div>
+
+                        </AliceCarousel>
+
+                    </div>
+
+                    <div className="mainHeader2">
+                        <div className="row">
+                            <div className="col-6">
+                                <img src="/assets/image/mainHeader2.png" alt="" className="w-100" />
+                            </div>
+                            <div className="col-6">
+                                <img src="/assets/image/mainHeader2.png" alt="" className="w-100" />
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* <h5 className='mb-4'><span className="blue">Top</span> Men</h5>
 
                     <AliceCarousel className='myCarousel' responsive={{
                         0: {
@@ -22,7 +70,7 @@ const Main = () => {
                         1200: {
                             items: 3
                         }
-                    }} mouseTracking autoPlay={true} autoPlayInterval={3500} disableDotsControls={true} infinite={true} className=''>
+                    }} mouseTracking autoPlay={true} autoPlayInterval={3500} disableDotsControls={true} infinite={true} >
 
                         <div className="carouselWrap ">
                             <div className='myCard cursor '>
@@ -157,7 +205,7 @@ const Main = () => {
                         1000: {
                             items: 3
                         }
-                    }} mouseTracking autoPlay={true} autoPlayInterval={3800} disableDotsControls={true} infinite={true} className=''>
+                    }} mouseTracking autoPlay={true} autoPlayInterval={3800} disableDotsControls={true} infinite={true} >
 
                         <div className="carouselWrap ">
                             <div className='myCard cursor '>
@@ -280,7 +328,7 @@ const Main = () => {
                             <img src="/assets/image/main33.png" className='w-100' alt="" />
                         </div>
 
-                    </AliceCarousel>
+                    </AliceCarousel> */}
 
                 </LeftLayout>
 
