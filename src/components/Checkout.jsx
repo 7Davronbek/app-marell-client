ca0e984988035ec793fde32985323170974e4594
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-03-24 01:56:59 +0500

    marell

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -7,7 +7,7 @@ const Checkout = () => {
             <LeftLayout>
                 <div className="checkout">
                     <div className="row">
-                        <div className="col-lg-6">
+                        <div className="col-lg-7">
                             <div className="cardsLeft">
                                 <div className="cardsBody">
                                     <div className="inputWrap">
@@ -54,14 +54,51 @@ const Checkout = () => {
                             </div>
                         </div>
 
-                        <div className="col-lg-6">
+                        <div className="col-lg-5">
                             <div className="cardsRight">
                                 <div className="cardsBody">
                                     <h2>Способ оплаты</h2>
                                     <div className="line"></div>
-                                    <button className="btn"><span><img src="/assets/image/visaCart.png" alt="" /></span> Карта</button>
+                                    <button className="btn"><span><img src="/assets/image/visaCart.png" alt="" /></span> <h6>Карта</h6> <span></span></button>
+                                    <button className="btn"><span><img src="/assets/image/eyeMini.png" alt="" /></span> <h1> AMARELL</h1> <span></span></button>
+
+                                    <div className="bottomLine"></div>
+
+                                    <div className="d-flex align-items-center justify-content-between mb-3">
+                                        <b>6 товаров на сумму</b>
+                                        <h3>9 600 P</h3>
+                                    </div>
+
+                                    <div className="d-flex align-items-center justify-content-between mb-3">
+                                        <b>6 товаров на сумму</b>
+                                        <h3>756 <span className="ml-2"><img src="/assets/image/eyeMini.png" alt="" /></span></h3>
+                                    </div>
+
+                                    <div className="d-flex align-items-center justify-content-between mb-3">
+                                        <b>Скидка</b>
+                                        <h3>0 P </h3>
+                                    </div>
+
+                                    <div className="d-flex align-items-center justify-content-between mb-3">
+                                        <b>Доставка</b>
+                                        <h3><span>Бесплатно</span></h3>
+                                    </div>
+
+                                    <div className="line"></div>
+
+                                    <div className="d-flex align-items-center justify-content-between mb-3">
+                                        <h1>Доставка</h1>
+                                        <h3>9 600 P</h3>
+                                    </div>
                                 </div>
                             </div>
+
+                            <div className="d-flex align-items-center">
+                                <span className="mr-2"><img src="/assets/image/check.png" alt="" /></span>
+                                <h2>Согласен с условиями Правил пользования торговой площадкой и правилами возврата товара</h2>
+                            </div>
+
+                            <button className="btn bottomBtn">Оплатить</button>
                         </div>
                     </div>
                 </div>
