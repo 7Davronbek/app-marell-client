bbb267b1b5801729b992dd806337e77aad6be840
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-04-02 00:37:07 +0500

    marell

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import LeftLayout from './LeftLayout'
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
+
+
+import 'react-accessible-accordion/dist/fancy-example.css';
 
 const Support = () => {
     return (
@@ -11,14 +21,80 @@ const Support = () => {
 
                 <p className="w-50">Подробно опишите возникшую проблему, мы постараемся  помочь вам в кротчайшее время.</p>
 
+                <div className="col-12">
+                    <Accordion>
+                        <AccordionItem data-aos='fade-right'>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>
+                                    What harsh truths do you prefer to ignore?
+                                </AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                <p>
+                                    Exercitation in fugiat est ut ad ea cupidatat ut in
+                                    cupidatat occaecat ut occaecat consequat est minim minim
+                                    esse tempor laborum consequat esse adipisicing eu
+                                    reprehenderit enim.
+                                </p>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                        <AccordionItem data-aos='fade-right'>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>
+                                    Is free will real or just an illusion?
+                                </AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                <p>
+                                    In ad velit in ex nostrud dolore cupidatat consectetur
+                                    ea in ut nostrud velit in irure cillum tempor laboris
+                                    sed adipisicing eu esse duis nulla non.
+                                </p>
+                            </AccordionItemPanel>
+                        </AccordionItem>
 
+                        <AccordionItem data-aos='fade-right'>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>
+                                    What harsh truths do you prefer to ignore?
+                                </AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                <p>
+                                    Exercitation in fugiat est ut ad ea cupidatat ut in
+                                    cupidatat occaecat ut occaecat consequat est minim minim
+                                    esse tempor laborum consequat esse adipisicing eu
+                                    reprehenderit enim.
+                                </p>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                        <AccordionItem data-aos='fade-right'>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>
+                                    Is free will real or just an illusion?
+                                </AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                <p>
+                                    In ad velit in ex nostrud dolore cupidatat consectetur
+                                    ea in ut nostrud velit in irure cillum tempor laboris
+                                    sed adipisicing eu esse duis nulla non.
+                                </p>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+
+                    </Accordion>
+
+                </div>
+
+                {/* 
                 <div className="btnWrap">
 
                     <Link className='a' to='/support/delivery-and-payment'>Доставка и Оплата</Link>
                     <Link className='a' to='/support/return-basis'>Условия Возврата</Link>
                     <Link className='a' to='/support/partnership'>Сотрудничество</Link>
 
-                </div>
+                </div> */}
             </LeftLayout>
 
         </div>
