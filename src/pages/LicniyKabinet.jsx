fa75373e98fed62ae4d597bbc83b42d98ec26e10
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-04-21 03:47:20 +0500

    marell

---
+++ src/pages/LicniyKabinet.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import FullLayout from '../components/FullLayout'
+
+const LicniyKabinet = () => {
+  return (
+    <>
+      <FullLayout>
+        <div className="licniyKabinet">
+          <p>Подписка даёт возможность участвовать в многоуровневой партнерской программе.
+
+            Приобретите подписку и зарабатывайте до 15% с покупок ваших партнеров.</p>
+        </div>
+      </FullLayout>
+    </>
+  )
+}
+
+export default LicniyKabinet
