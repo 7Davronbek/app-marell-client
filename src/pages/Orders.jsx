1bce9aa6a51be43f96193a3f6cc64f4c39525031
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-04-28 07:45:01 +0500

    marell

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,12 +1,261 @@
 import React from 'react'
 import FullLayout from '../components/FullLayout'
+import { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const Orders = () => {
     return (
         <>
             <FullLayout>
                 <div className="orders">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, culpa rerum? Architecto quam in odit accusantium, magnam nemo non dolore, tempora minima incidunt laudantium cumque quidem dolor cum autem dolorem!
+                    <div className="row">
+                        <div className="col-12">
+                            <h1>18-20 марта</h1>
+                        </div>
+                        <div className="col-12 mt-3">
+
+                            <Swiper
+                                navigation={true}
+                                slidesPerView={4}
+                                centeredSlides={false}
+                                spaceBetween={10}
+                                grabCursor={true}
+                                pagination={{
+                                    clickable: true,
+                                }}
+                                modules={[Pagination]}
+                                className="mySwiper"
+                            >
+                                <SwiperSlide>
+
+                                    <div className="carouselWrap ">
+                                        <div className="cards">
+
+                                            <div className="cardsHeader">
+                                                <img src="/assets/image/orders.png" alt="" className="w-100" />
+                                            </div>
+                                            <p>Собран</p>
+                                        </div>
+                                    </div>
+
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+
+                                    <div className="carouselWrap ">
+                                        <div className="cards">
+
+                                            <div className="cardsHeader">
+                                                <img src="/assets/image/orders.png" alt="" className="w-100" />
+                                            </div>
+                                            <p>Отсортирован</p>
+                                        </div>
+                                    </div>
+
+
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+                                    <div className="carouselWrap ">
+                                        <div className="cards">
+
+                                            <div className="cardsHeader">
+                                                <img src="/assets/image/orders.png" alt="" className="w-100" />
+                                            </div>
+                                            <p>Отсортирован</p>
+                                        </div>
+                                    </div>
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+
+                                    <div className="carouselWrap ">
+                                        <div className="cards">
+
+                                            <div className="cardsHeader">
+                                                <img src="/assets/image/orders.png" alt="" className="w-100" />
+                                            </div>
+                                            <p>В пути</p>
+                                        </div>
+                                    </div>
+
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+
+                                    <div className="carouselWrap ">
+                                        <div className="cards">
+
+                                            <div className="cardsHeader">
+                                                <img src="/assets/image/orders.png" alt="" className="w-100" />
+                                            </div>
+                                            <p>В пути</p>
+                                        </div>
+                                    </div>
+
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+
+                                    <div className="carouselWrap ">
+                                        <div className="cards">
+
+                                            <div className="cardsHeader">
+                                                <img src="/assets/image/orders.png" alt="" className="w-100" />
+                                            </div>
+                                            <p>В пути</p>
+                                        </div>
+                                    </div>
+
+                                </SwiperSlide>
+
+
+                            </Swiper>
+                        </div>
+
+                        <div className="col-12 mt-5">
+                            <h1>Полученные</h1>
+                        </div>
+                        <div className="col-12 mt-3">
+
+                            <Swiper
+                                navigation={true}
+                                slidesPerView={4}
+                                centeredSlides={false}
+                                spaceBetween={10}
+                                grabCursor={true}
+                                pagination={{
+                                    clickable: true,
+                                }}
+                                modules={[Pagination]}
+                                className="mySwiper"
+                            >
+                                <SwiperSlide>
+
+                                    <div className="carouselWrap ">
+                                        <div className="cards">
+
+                                            <div className="cardsHeader">
+                                                <img src="/assets/image/orders.png" alt="" className="w-100" />
+                                            </div>
+                                            <div className="d-flex align-items-center my-3">
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                            </div>
+                                            <p>Отзыв <span><img src="/assets/icon/camera.svg" alt="" /></span></p>
+                                        </div>
+                                    </div>
+
+                                </SwiperSlide>
+
+                                <SwiperSlide>
+
+                                    <div className="carouselWrap ">
+                                        <div className="cards">
+
+                                            <div className="cardsHeader">
+                                                <img src="/assets/image/orders.png" alt="" className="w-100" />
+                                            </div>
+                                            <div className="d-flex align-items-center my-3">
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                            </div>
+                                            <p>Отзыв <span><img src="/assets/icon/camera.svg" alt="" /></span></p>
+                                        </div>
+                                    </div>
+
+                                </SwiperSlide>
+                                <SwiperSlide>
+
+                                    <div className="carouselWrap ">
+                                        <div className="cards">
+
+                                            <div className="cardsHeader">
+                                                <img src="/assets/image/orders.png" alt="" className="w-100" />
+                                            </div>
+                                            <div className="d-flex align-items-center my-3">
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                            </div>
+                                            <p>Отзыв <span><img src="/assets/icon/camera.svg" alt="" /></span></p>
+                                        </div>
+                                    </div>
+
+                                </SwiperSlide>
+                                <SwiperSlide>
+
+                                    <div className="carouselWrap ">
+                                        <div className="cards">
+
+                                            <div className="cardsHeader">
+                                                <img src="/assets/image/orders.png" alt="" className="w-100" />
+                                            </div>
+                                            <div className="d-flex align-items-center my-3">
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                            </div>
+                                            <p>Отзыв <span><img src="/assets/icon/camera.svg" alt="" /></span></p>
+                                        </div>
+                                    </div>
+
+                                </SwiperSlide>
+                                <SwiperSlide>
+
+                                    <div className="carouselWrap ">
+                                        <div className="cards">
+
+                                            <div className="cardsHeader">
+                                                <img src="/assets/image/orders.png" alt="" className="w-100" />
+                                            </div>
+                                            <div className="d-flex align-items-center my-3">
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                            </div>
+                                            <p>Отзыв <span><img src="/assets/icon/camera.svg" alt="" /></span></p>
+                                        </div>
+                                    </div>
+
+                                </SwiperSlide>
+                                <SwiperSlide>
+
+                                    <div className="carouselWrap ">
+                                        <div className="cards">
+
+                                            <div className="cardsHeader">
+                                                <img src="/assets/image/orders.png" alt="" className="w-100" />
+                                            </div>
+                                            <div className="d-flex align-items-center my-3">
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                                <i className="icon icon-star mr-1"></i>
+                                            </div>
+                                            <p>Отзыв <span><img src="/assets/icon/camera.svg" alt="" /></span></p>
+                                        </div>
+                                    </div>
+
+                                </SwiperSlide>
+
+
+                            </Swiper>
+                        </div>
+                    </div>
                 </div>
             </FullLayout>
         </>
