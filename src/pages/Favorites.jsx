476a20edcdd0e9b115acb0ba1e2f4ffe0e7f0730
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-05-24 03:06:11 +0500

    marell

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -7,73 +7,73 @@ const Favorites = () => {
             <FullLayout>
                 <div className="favorites">
                     <div className="row">
-                        <div className="col-3">
+                        <div className="col-lg-3 px-1 px-lg-2 col-6">
                             <div className="cards">
                                 <img src="/assets/image/favorite.png" className='w-100' alt="" />
                                 <div className="icon icon-heart"></div>
                             </div>
                         </div>
-                        <div className="col-3">
+                        <div className="col-lg-3 px-1 px-lg-2 col-6">
                             <div className="cards">
                                 <img src="/assets/image/favorite.png" className='w-100' alt="" />
                                 <div className="icon icon-heart"></div>
                             </div>
                         </div>
-                        <div className="col-3">
+                        <div className="col-lg-3 px-1 px-lg-2 col-6">
                             <div className="cards">
                                 <img src="/assets/image/favorite.png" className='w-100' alt="" />
                                 <div className="icon icon-heart"></div>
                             </div>
                         </div>
-                        <div className="col-3">
+                        <div className="col-lg-3 px-1 px-lg-2 col-6">
                             <div className="cards">
                                 <img src="/assets/image/favorite.png" className='w-100' alt="" />
                                 <div className="icon icon-heart"></div>
                             </div>
                         </div>
-                        <div className="col-3">
+                        <div className="col-lg-3 px-1 px-lg-2 col-6">
                             <div className="cards">
                                 <img src="/assets/image/favorite.png" className='w-100' alt="" />
                                 <div className="icon icon-heart"></div>
                             </div>
                         </div>
-                        <div className="col-3">
+                        <div className="col-lg-3 px-1 px-lg-2 col-6">
                             <div className="cards">
                                 <img src="/assets/image/favorite.png" className='w-100' alt="" />
                                 <div className="icon icon-heart"></div>
                             </div>
                         </div>
-                        <div className="col-3">
+                        <div className="col-lg-3 px-1 px-lg-2 col-6">
                             <div className="cards">
                                 <img src="/assets/image/favorite.png" className='w-100' alt="" />
                                 <div className="icon icon-heart"></div>
                             </div>
                         </div>
-                        <div className="col-3">
+                        <div className="col-lg-3 px-1 px-lg-2 col-6">
                             <div className="cards">
                                 <img src="/assets/image/favorite.png" className='w-100' alt="" />
                                 <div className="icon icon-heart"></div>
                             </div>
                         </div>
-                        <div className="col-3">
+                        <div className="col-lg-3 px-1 px-lg-2 col-6">
                             <div className="cards">
                                 <img src="/assets/image/favorite.png" className='w-100' alt="" />
                                 <div className="icon icon-heart"></div>
                             </div>
                         </div>
-                        <div className="col-3">
+                        <div className="col-lg-3 px-1 px-lg-2 col-6">
                             <div className="cards">
                                 <img src="/assets/image/favorite.png" className='w-100' alt="" />
                                 <div className="icon icon-heart"></div>
                             </div>
                         </div>
-                        <div className="col-3">
+                        <div className="col-lg-3 px-1 px-lg-2 col-6">
                             <div className="cards">
                                 <img src="/assets/image/favorite.png" className='w-100' alt="" />
                                 <div className="icon icon-heart"></div>
                             </div>
                         </div>
-                        <div className="col-3">
+                        <div className="col-lg-3 px-1 px-lg-2 col-6">
                             <div className="cards">
                                 <img src="/assets/image/favorite.png" className='w-100' alt="" />
                                 <div className="icon icon-heart"></div>
@@ -82,9 +82,10 @@ const Favorites = () => {
                     </div>
 
                     <div className="row">
-                        <div className="col-12 mt-5 d-flex align-items-center justify-content-between">
-                            <button className="btn">Товары</button>
-                            <span><img src="/assets/image/pagination.png" alt="" /></span>
+                        <div className="col-12 mt-lg-5 mt-4 d-flex align-items-center justify-content-between flex-column">
+                            <button className="btn first">Товары</button>
+                            <span><img src="/assets/image/pagination.png" className='w-100' alt="" /></span>
+                            <button className="btn mobile">Товары</button>
                             <button className="btn pinks">Бренды</button>
                         </div>
                     </div>
